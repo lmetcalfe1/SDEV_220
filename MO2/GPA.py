58d8a3cb9ef555e5ed8ce4ee2709d68b1b60f2b1
Logan Metcalfe <lmetcalfe1@ivytech.edu>
2024-06-13 17:02:05 -0400

    Added comments to MO2 Case study

--- MO2/GPA.py
+++ MO2/GPA.py
@@ -1,18 +1,18 @@
 #Logan Metcalfe
 #GPA calculator
 #This program will take a name and GPA input and calculate whether or not it made the deans list, or honor roll.
-deansList = float(3.5)
+deansList = float(3.5) #initiallized the values for these two variables.
 honors = float(3.25)
-name =  str(input("Enter the name of the student: "))
-while name != "zzz":
-    try:
-        gpa = float(input("input the GPA for the student: "))
-        if gpa >= deansList:
-            print(name + " qualifies for Deans List.")
-        elif gpa < deansList and gpa > honors:
-            print(name + " qualified for Honors.")
-        else:
+name =  str(input("Enter the name of the student: ")) #First input, can use zzz. ZZZ will not close the loop.
+while name != "zzz": #While loop
+    try: #Basic input validation
+        gpa = float(input("input the GPA for the student: ")) #Second input
+        if gpa >= deansList: #Determines the first condition, which is greater than 3.5
+            print(name + " qualifies for Deans List.") #output
+        elif gpa < deansList and gpa > honors: #Determines second condition, greater than 3.25, but less than 3.5
+            print(name + " qualified for Honors.") #output
+        else: #Anything else did not qualify 
             print (name + " did not qualify for Deans List or Honors.")
-        name =  str(input("Enter the name of the student: "))
+        name =  str(input("Enter the name of the student: ")) #loop restarts
     except:
-        print("invalid Input.")
+        print("invalid Input.") #outputs when an invalid input is sent for the GPA (not a number)
