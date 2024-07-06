135039707365be999935d62ffef48523a76d8b42
Logan Metcalfe <lmetcalfe1@ivytech.edu>
2024-07-05 22:15:39 -0400

    Something something update blah blah blah

---
+++ MO5_test_discussion.py
@@ -0,0 +1,12 @@
+import requests
+import webbrowser
+endpoint = "https://api.nasa.gov/planetary/apod"
+api_key = "5sjIPiQHxVEc5vyGCvXI3hdQFOHndbwi2h956s5A"
+query_params = {"api_key": api_key}
+response = requests.get(endpoint, params= query_params)
+response.text
+print(response.text)
+picture = response.json()
+url = picture["hdurl"]
+webbrowser.open(url, new=2, autoraise=True)
+# https://realpython.com/python-api/
